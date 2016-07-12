f87675eed8bdf1ecf1b0774efc42ce368bfd76c8
Dusan Jovanovic <djovanovic@maxeler.com>
2016-07-12 17:23:57 +0200

    Updated CI and build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env sh
 
-mvn -P build-individual-bundles install -f eclipse-platform-parent/pom.xml
+# Fetch submodules
+git submodule update --init eclipse.platform.releng.aggregator
+cd eclipse.platform.releng.aggregator
+git submodule update --init eclipse.jdt.core
+git submodule update --init eclipse.jdt.ui
+
+# Build plugins
 mvn -P build-individual-bundles install -f eclipse.jdt.core/pom.xml
-mvn -P build-individual-bundles install -f eclipse.jdt.core/org.eclipse.jdt.core/pom.xml
-
-git clone -b eclipse-3.10.2-SNAPSHOT https://github.com/maxeler/plexus-compiler
-mvn install -f plexus-compiler/pom.xml
-mvn install -f plexus-compiler/plexus-compiler-api/pom.xml
-mvn install -f plexus-compiler/plexus-compilers/pom.xml
-mvn install -f plexus-compiler/plexus-compilers/plexus-compiler-eclipse/pom.xml
-
-mvn -P build-individual-bundles install -f eclipse.jdt.ui/org.eclipse.jdt.ui/pom.xml
-
-mvn test -f examples/pom.xml
+mvn -P build-individual-bundles install -f eclipse.jdt.ui/pom.xml
